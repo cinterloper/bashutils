bd8887fdf459f8cfb3b29e7fac66bbb0f70cca33
grant@iowntheinter.net <grant@iowntheinter.net>
2016-02-21 21:02:36 -0700

    scrape the scraps together

---
+++ arguments.sh
@@ -0,0 +1,31 @@
+#CREDIT: https://raw.githubusercontent.com/mattly/bork/master/lib/helpers/arguments.sh
+arguments () {
+  op=$1
+  shift
+  case $op in
+    get)
+      key=$1
+      shift
+      value=
+      while [ -n "$1" ] && [ -z "$value" ]; do
+        this=$1
+        shift
+        if [ ${this:0:2} = '--' ]; then
+          tmp=${this:2}       # strip off leading --
+          echo "$tmp" | grep -E '=' > /dev/null
+          if [ "$?" -eq 0 ]; then
+            param=${tmp%%=*}    # everything before =
+            val=${tmp##*=}      # everything after =
+          else
+            param=$tmp
+            val="true"
+          fi
+        if [ "$param" = $key ]; then value=$val; fi
+        fi
+      done
+      [ -n $value ] && echo "$value"
+      ;;
+    *) return 1 ;;
+  esac
+}
+
---
+++ testvars.sh
@@ -0,0 +1,13 @@
+#CREDIT:cinterloper
+#declare your variables like so then call this method to test they have been declared by the user:
+#VARS=(THIS THAT SOMETHINGELSE)
+testvars () {
+ result=0
+ for var in ${VARS[@]}
+ do
+  if [  "$(eval echo \$"$var")" == "" ]
+  then
+    result=-1
+  fi
+ done
+}
